017c7e8e6324bb8db59b8c3c53dcb15e4a897bec
suzuki3jp <99058480+suzuki3jp@users.noreply.github.com>
2023-01-20 06:33:41 +0900

    Router実装まで

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
+import { Logger } from '@suzuki3jp/utils';
+
 import { DataManager } from './class/DataManager';
+import { router } from './Router/Router';
 import { setupServer } from './utils/Express';
 
 (async () => {
     const config = new DataManager().getConfig();
-    const server = setupServer(config.isSecure);
+    const servers = setupServer(config.isSecure);
+    const logger = new Logger({ isSaveLogToCsv: false });
+    servers.server.listen(config.port, () =>
+        logger.system(
+            'API is ready at ',
+            config.isSecure ? 'https' : 'http',
+            '://localhost:',
+            config.port.toString(),
+            '.'
+        )
+    );
+    servers.app.use('/', router);
 })();
--- src/utils/Express.ts
+++ src/utils/Express.ts
@@ -1,25 +1,32 @@
 // import packages
 import express from 'express';
+import type { Express } from 'express';
 import { createServer as httpCreateServer, Server as HTTP } from 'http';
 import { createServer as httpsCreateServer, Server as HTTPS } from 'https';
 
 // import modules
 import { DataManager } from '../class/DataManager';
 
-export function setupServer(isSecure: boolean): Servers {
+export function setupServer(isSecure: boolean): { app: Express; server: Servers } {
     const DM = new DataManager();
     const app = express();
 
     if (isSecure) {
-        return httpsCreateServer(
-            {
-                key: DM.getKey(),
-                cert: DM.getCert(),
-            },
-            app
-        );
+        return {
+            app,
+            server: httpsCreateServer(
+                {
+                    key: DM.getKey(),
+                    cert: DM.getCert(),
+                },
+                app
+            ),
+        };
     } else {
-        return httpCreateServer(app);
+        return {
+            app,
+            server: httpCreateServer(app),
+        };
     }
 }
 
